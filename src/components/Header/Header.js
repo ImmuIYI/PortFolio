907abcff83be86b6c77f58d76a645753e050490d
Immu <imran.gdr26@gmail.com>
2023-02-27 18:05:55 +0530

    first commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,7 @@ const Header = () =>  (
     <Div1>
       <Link href="/">
         <a style={{display: "flex", alignItems: "center", color: 'white', marginBottom: '15px'}}>
-          <DiCssdeck size="3rem" /> <Span>PortFolio</Span>
+          <DiCssdeck size="3rem" /> <Span>Immufolio</Span>
         </a>
       </Link>
     </Div1>
